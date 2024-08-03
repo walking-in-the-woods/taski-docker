98fdb1ce2d3d280ab35c7bb3f379a9db4fd29306
Alexander Semenov <AlexPskDesign@yandex.ru>
2024-08-03 09:46:13 +0300

    Corrected after PEP8 check 3

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,6 @@
+from rest_framework import routers
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
 from api import views
 
 router = routers.DefaultRouter()
